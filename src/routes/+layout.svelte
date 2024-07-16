f66368440da7673b15bc5bd112ef1755e8d73cac
dawidseipold <seipold.dawid@gmail.com>
2024-07-16 11:27:26 +0200

    build: add mastercss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Fragment } from '@master/css.svelte';
+	import { onMount } from 'svelte';
+	import '$styles/global.css';
+
+	let CSSRuntimeProvider = $state(Fragment as any);
+
+	onMount(async () => {
+		CSSRuntimeProvider = (await import('@master/css.svelte')).default;
+	});
+
+	const { children } = $props();
+</script>
+
+<svelte:component this={CSSRuntimeProvider} config={import('../../master.css')}>
+	{@render children()}
+</svelte:component>
