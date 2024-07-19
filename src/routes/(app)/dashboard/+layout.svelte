eeaf5f56db257091fad6cb588a1e1e7f64774638
dawidseipold <seipold.dawid@gmail.com>
2024-07-19 12:11:18 +0200

    feat: add basic routes for the app feat: add layouts for dashboard and app

---
+++ src/routes/(app)/dashboard/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Sidebar } from '$components/layout/sidebar';
+	import type { Snippet } from 'svelte';
+
+	interface DashboardLayoutProps {
+		children: Snippet;
+	}
+
+	let { children }: DashboardLayoutProps = $props();
+</script>
+
+<div class="bg:base-100 min-h:100dvh flex gap:24 p:16">
+	<Sidebar />
+
+	<main class="bg:base-200 w:100% r:16 p:24">
+		{@render children()}
+	</main>
+</div>
---
+++ src/routes/(app)/dashboard/insights/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Insights</h1>
