84195f05b82e2cdeb83639283129cc9be45dd589
dawidseipold <seipold.dawid@gmail.com>
2024-07-26 12:34:50 +0200

    refacotr: prepare the sidebar component to receive the page data and actions for the form

--- src/components/layout/sidebar/index.ts
+++ src/components/layout/sidebar/index.ts
@@ -1,37 +1,40 @@
+import type { PageData } from '../../../routes/(app)/dashboard/$types';
 import Root from './sidebar.svelte';
 import { Compass, Home, PieChart } from 'lucide-svelte';
 
 const CHOICES = [
-    {
-        value: 'poll',
-        label: 'Poll',
-        description: 'A super nice description that tells you all.'
-    },
-    {
-        value: 'quiz',
-        label: 'Quiz',
-        description: 'A super nice description that tells you all.'
-    }
+	{
+		value: 'poll',
+		label: 'Poll',
+		description: 'A super nice description that tells you all.'
+	},
+	{
+		value: 'quiz',
+		label: 'Quiz',
+		description: 'A super nice description that tells you all.'
+	}
 ];
 
 const PAGES = [
-    {
-        name: 'Home',
-        path: '/dashboard',
-        icon: Home
-    },
-    {
-        name: 'Discover',
-        path: '/dashboard/discover',
-        icon: Compass
-    },
-    {
-        name: 'Insights',
-        path: '/dashboard/insights',
-        icon: PieChart
-    }
+	{
+		name: 'Home',
+		path: '/dashboard',
+		icon: Home
+	},
+	{
+		name: 'Discover',
+		path: '/dashboard/discover',
+		icon: Compass
+	},
+	{
+		name: 'Insights',
+		path: '/dashboard/insights',
+		icon: PieChart
+	}
 ] as const;
 
-interface Props {}
+interface Props {
+	data: PageData;
+}
 
 export { Root as Sidebar, CHOICES, PAGES, type Props as SidebarProps };
--- src/components/layout/sidebar/sidebar.svelte
+++ src/components/layout/sidebar/sidebar.svelte
@@ -1,18 +1,29 @@
 <script lang="ts">
 	import { Dialog } from '$components/actions/dialog';
-	import { DialogTabs } from '$components/actions/dialog/tabs'
 	import { Logo } from '$components/data-display/logo';
 	import { cn } from '@kurasu/variants';
-	import { Check, Compass, Home, PieChart, type Icon as IconType } from 'lucide-svelte';
 	import { page } from '$app/stores';
+	import { zodClient } from 'sveltekit-superforms/adapters';
 	import { Button } from '$components/actions/button';
-	import { PAGES, CHOICES } from '.';
+	import { PAGES, CHOICES, type SidebarProps } from '.';
 	import { ChoiceGroup } from '$components/data-input/choice-group';
+	import SuperDebug, { superForm } from 'sveltekit-superforms';
+	import type { Icon as IconType } from 'lucide-svelte';
+	import { createNewDialogSchema } from '$lib/schemas';
+	import { Control, Field, Label, Description, FieldErrors, Fieldset, Legend } from 'formsnap';
 
 	let pathname = $derived($page.url.pathname);
 
-	let dialogOpen = $state(false)
-	let activeDialogChoice = $state(CHOICES[0].value)
+	let dialogOpen = $state(false);
+	let activeDialogChoice = $state(CHOICES[0].value);
+
+	let { data }: SidebarProps = $props();
+
+	const form = superForm(data.form, {
+		validators: zodClient(createNewDialogSchema)
+	});
+
+	const { form: formData, enhance } = form;
 </script>
 
 <div class="flex flex:col min-w:256 gap:32">
@@ -21,39 +32,52 @@
 	<nav class="flex flex:col gap:16">
 		<Dialog
 			bind:openState={dialogOpen}
-			title='Create something new'
-			description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae libero bibendum leo imperdiet imperdiet convallis a lacus. Donec fringilla volutpat nunc et sollicitudin.'
+			title="Create something new"
+			description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae libero bibendum leo imperdiet imperdiet convallis a lacus. Donec fringilla volutpat nunc et sollicitudin."
 			alertBeforeClose
 			closeOnEvents
 		>
-			{#snippet trigger({builder})}
+			{#snippet trigger({ builder })}
 				<Button builders={[builder]}>Create New</Button>
 			{/snippet}
 
 			<div class="flex flex:col gap-y:16">
-				<form
-					onsubmit={(e) => {
-						e.preventDefault()
-						console.log('submitted')
-					}}
-					class='flex flex:col gap:16 w:100%'
-				>
-					<div class="flex flex:col gap:6 w:100%">
-						<label for="title" class='font:16 font:semibold'>Title</label>
-						<input id='title' type="text" class='flex px:16 py:12 r:16 b:base-300|solid|1 color:neutral::placeholder' placeholder='Title'>
-					</div>
+				<form method="POST" action="/dashboard" use:enhance class="flex flex:col gap:16 w:100%">
+					<Field {form} name="title">
+						<Control let:attrs>
+							<div class="flex flex:col gap:6 w:100%">
+								<Label>Title</Label>
+								<input
+									{...attrs}
+									type="text"
+									bind:value={$formData.title}
+									class="flex px:16 py:12 r:16 b:base-300|solid|1 color:neutral::placeholder"
+									placeholder="Title"
+								/>
+								<FieldErrors />
+							</div>
+						</Control>
+					</Field>
 
-					<div class="flex flex:col gap:6 w:100%">
-						<label for="type" class='font:16 font:semibold'>Type</label>
-						<ChoiceGroup id='type' choices={CHOICES} bind:value={activeDialogChoice} orientation='horizontal' />
-					</div>
+					<Fieldset {form} name="type">
+						<div class="flex flex:col gap:6 w:100%">
+							<Legend>Select the type of content you want to create:</Legend>
 
-					<Button type='submit' size='small' class='as:end w:max-content'>
-						Next
-					</Button>
+							<ChoiceGroup
+								id="type"
+								choices={CHOICES}
+								bind:value={$formData.type}
+								orientation="horizontal"
+							/>
+
+							<FieldErrors />
+						</div>
+					</Fieldset>
+
+					<Button type="submit" size="small" class="as:end w:max-content">Next</Button>
 				</form>
 
-				
+				<SuperDebug data={$formData} />
 			</div>
 		</Dialog>
 
