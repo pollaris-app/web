074c282dc2bd6f123591df1b15095bbe66bf2027
dawidseipold <seipold.dawid@gmail.com>
2024-08-06 13:04:42 +0200

    feat: add an additional sidebar for settings (to be refactored into a reusable component since more pages might use this layout)

---
+++ src/components/layout/sidebar-settings/index.ts
@@ -0,0 +1,56 @@
+import Root from './sidebar-settings.svelte';
+
+const CONTENT = [
+	{
+		id: 1,
+		value: 'overview',
+		label: 'Overview',
+		href: function () {
+			return `/dashboard/settings`;
+		}
+	},
+	{
+		id: 2,
+		value: 'account',
+		label: 'Account',
+		href: function () {
+			return `/dashboard/settings/${this.value}`;
+		}
+	},
+	{
+		id: 3,
+		value: 'appearance',
+		label: 'Appearance',
+		href: function () {
+			return `/dashboard/settings/${this.value}`;
+		}
+	},
+	{
+		id: 4,
+		value: 'notifications',
+		label: 'Notifications',
+		href: function () {
+			return `/dashboard/settings/${this.value}`;
+		}
+	},
+	{
+		id: 5,
+		value: 'billing',
+		label: 'Billing',
+		href: function () {
+			return `/dashboard/settings/${this.value}`;
+		}
+	},
+	{
+		id: 6,
+		value: 'security',
+		label: 'Security',
+		href: function () {
+			return `/dashboard/settings/${this.value}`;
+		}
+	}
+];
+
+interface Props {}
+
+export { Root as SidebarSettings, CONTENT, type Props as SidebarSettingsProps };
---
+++ src/components/layout/sidebar-settings/sidebar-settings.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Search } from 'lucide-svelte';
+	import { CONTENT } from '.';
+</script>
+
+<nav class="flex flex:col gap-y:16 bg:base-200 max-w:256">
+	<h2 class="color:white font:bold font:24">Settings</h2>
+
+	<label for="search" class="rel">
+		<Search class="abs left:12 top:12 color:neutral" />
+		<input
+			id="search"
+			type="text"
+			class="w:100% font:18 pr:16 pl:40 py:12 b:2|solid|base-400 r:16 h:48 color:neutral"
+			placeholder="Search"
+		/>
+	</label>
+
+	<ul class="flex flex:col gap-y:4 font:semibold letter-spacing:0.5 cursor:pointer">
+		{#each CONTENT as item}
+			<li class="color:neutral color:white:hover bg:base-300:hover px:16 py:12 r:16">
+				<a href={item.href()}>{item.label}</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
