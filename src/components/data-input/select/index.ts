4796f55d54be77569475cd84cfadae7f3a44310a
dawidseipold <seipold.dawid@gmail.com>
2024-08-23 08:02:19 +0200

    feat: add an optional icon indicator to a select element

--- src/components/data-input/select/index.ts
+++ src/components/data-input/select/index.ts
@@ -1,12 +1,20 @@
 import Root from './select.svelte';
-import type { Selected, SelectProps } from 'bits-ui';
+
+import type { IconProps } from '$components/icon';
+import type { SelectProps } from 'bits-ui';
+
+type Item<T> = {
+	value: T;
+	label: string;
+	indicator?: IconProps;
+};
 
 type Props<T, M extends boolean> = SelectProps<T, M> & {
 	data: string[];
-	items: Selected<T>[];
+	items: Item<T>[];
 	placeholder: string;
 	multiple?: M;
 	class?: string;
 };
 
-export { Root as Select, type Props as SelectProps };
+export { Root as Select, type Props as SelectProps, type Item as SelectItem };
--- src/components/data-input/select/select.svelte
+++ src/components/data-input/select/select.svelte
@@ -2,6 +2,8 @@
 	import { Select, type Selected } from 'bits-ui';
 	import { Control } from 'formsnap';
 	import type { SelectProps } from '.';
+	import { Check, ChevronsUpDown } from 'lucide-svelte';
+	import { Icon, type IconProps } from '$components/icon';
 
 	let {
 		data = $bindable(),
@@ -31,6 +33,10 @@
 	}) as [M] extends [true] ? Selected<T>[] | undefined : Selected<T> | undefined;
 </script>
 
+{#snippet accentIndicator(props: IconProps)}
+	<Icon {...props} />
+{/snippet}
+
 <Control let:attrs>
 	<Select.Root
 		{selected}
@@ -54,9 +60,10 @@
 
 		<Select.Trigger
 			{...attrs}
-			class="flex ai:center jc:start w:256 font:18 py:8 px:12 b:2|solid|base-400 r:16 h:48 color:neutral"
+			class="flex ai:center jc:space-between w:256 font:18 py:8 px:12 b:2|solid|base-400 r:16 h:48 color:neutral"
 		>
 			<Select.Value {placeholder} />
+			<ChevronsUpDown size={16} />
 		</Select.Trigger>
 
 		<Select.Content class="w:100% mt:8 p:4 max-w:256 r:16 outline:2|solid|base-400 bg:base-200">
@@ -64,8 +71,20 @@
 				<Select.Item
 					value={item.value}
 					label={item.label}
-					class="flex gap:8 ai:center py:8 px:12 r:12 cursor:pointer color:neutral font:medium color:primary:hover bg:base-300:hover"
-				/>
+					class="flex gap:8 ai:center jc:start py:8 px:12 r:12 cursor:pointer color:neutral font:medium color:primary:hover bg:base-300:hover"
+					let:isSelected
+				>
+					{#if item.indicator}
+						{@render accentIndicator(item.indicator)}
+					{/if}
+					<span>
+						{item.label}
+					</span>
+
+					{#if isSelected}
+						<Check size={16} class="ml:auto" />
+					{/if}
+				</Select.Item>
 			{/each}
 		</Select.Content>
 	</Select.Root>
