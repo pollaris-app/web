57e9a4d1424f57ef9694f16ec6f7af45aa5285ea
dawidseipold <seipold.dawid@gmail.com>
2024-08-24 00:24:23 +0200

    feat: add error handling to settings section chore: change styling of settings section a little

--- src/components/layout/settings/section/settings-section.svelte
+++ src/components/layout/settings/section/settings-section.svelte
@@ -1,5 +1,5 @@
 <script lang="ts" generics="T extends typeof form">
-	import { Description, Fieldset, Legend } from 'formsnap';
+	import { Description, FieldErrors, Fieldset, Legend } from 'formsnap';
 	import type { SettingsSectionProps } from '.';
 	import { Separator } from '$components/layout/separator';
 
@@ -10,12 +10,23 @@
 	<Separator />
 
 	<Fieldset name={fieldName} {form}>
-		<div class="flex gap:32 py:16">
+		<div class="flex gap:32 py:16 jc:space-between ai:start">
 			<div class="flex flex:col min-w:196">
 				<Legend class="color:primary f:18 font:semibold">{legend.title}</Legend>
 				<Description class="color:neutral">{legend.description}</Description>
 			</div>
+
 			{@render children()}
 		</div>
+
+		<FieldErrors let:errors let:errorAttrs asChild>
+			<ul>
+				{#each errors as error}
+					<li class="fg:error" {...errorAttrs}>
+						{error}
+					</li>
+				{/each}
+			</ul>
+		</FieldErrors>
 	</Fieldset>
 </section>
