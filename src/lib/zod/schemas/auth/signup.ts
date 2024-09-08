84e5c4d706f4841f067af451ca4867a2c3cead63
dawidseipold <seipold.dawid@gmail.com>
2024-09-08 22:59:22 +0200

    feat: add signUpEmailSchema and signUpPasswordSchema

---
+++ src/lib/zod/schemas/auth/signup.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+export const signUpEmailSchema = z.object({
+	email: z.string().email(),
+	agreement: z.literal(true, {
+		errorMap: () => ({ message: 'You must agree to the terms and conditions.' })
+	})
+});
+
+export const signUpPasswordSchema = z
+	.object({
+		password: z.string().min(8).max(64),
+		confirmPassword: z.string().min(8).max(64)
+	})
+	.superRefine(({ password, confirmPassword }, ctx) => {
+		if (password !== confirmPassword) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: 'Passwords do not match',
+				path: ['confirmPassword']
+			});
+		}
+	});
