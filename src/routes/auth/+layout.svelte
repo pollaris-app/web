62d40513afd0673eb7dc2020c152142333f3bffd
dawidseipold <seipold.dawid@gmail.com>
2024-09-09 01:11:54 +0200

    feat: add auth layouts

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
+
+<main class="bg:base-50 fg:neutral w:100% h:100dvh flex p:32 gap:32">
+	{@render children()}
+</main>
