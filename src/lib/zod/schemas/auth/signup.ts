d1282910ebf2ad29253ea31bebd4ae0a3730cb18
dawidseipold <seipold.dawid@gmail.com>
2024-09-13 04:06:32 +0200

    refacor: in signUpSchema replace zod with valibot

--- src/lib/zod/schemas/auth/signup.ts
+++ src/lib/zod/schemas/auth/signup.ts
@@ -1,23 +1,31 @@
-import { z } from 'zod';
+import * as v from 'valibot';
 
-export const signUpEmailSchema = z.object({
-	email: z.string().email()
-});
+const EmailSchema = v.pipe(
+	v.string(),
+	v.nonEmpty('Please enter your email.'),
+	v.email('The email is badly formatted.'),
+	v.maxLength(30, 'Your email is too long.')
+);
 
-export const signUpPasswordSchema = z
-	.object({
-		password: z.string().min(8).max(64),
-		confirmPassword: z.string().min(8).max(64),
-		agreement: z.literal<boolean>(true, {
-			errorMap: () => ({ message: 'You must agree to the terms and conditions.' })
-		})
-	})
-	.superRefine(({ password, confirmPassword }, ctx) => {
-		if (password !== confirmPassword) {
-			ctx.addIssue({
-				code: z.ZodIssueCode.custom,
-				message: 'Passwords do not match',
-				path: ['confirmPassword']
-			});
-		}
-	});
+const PasswordSchema = v.pipe(
+	v.string(),
+	v.minLength(8, 'Password must be at least 8 characters long.'),
+	v.maxLength(64, 'Password must be at most 64 characters long.')
+);
+
+export const signUpSchema = v.pipe(
+	v.object({
+		email: EmailSchema,
+		password: PasswordSchema,
+		confirmPassword: v.string(),
+		agreement: v.literal(true as boolean, 'You must agree to the terms and conditions.')
+	}),
+	v.forward(
+		v.partialCheck(
+			[['password'], ['confirmPassword']],
+			(input) => input.password === input.confirmPassword,
+			'Passwords do not match.'
+		),
+		['confirmPassword']
+	)
+);
