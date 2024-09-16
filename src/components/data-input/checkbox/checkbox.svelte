603628cc6e1a22edfe73b61a7130bc638258c1f9
dawidseipold <seipold.dawid@gmail.com>
2024-09-17 00:20:09 +0200

    fix: make the checkbox fully working with superforms (errors and state)

--- src/components/data-input/checkbox/checkbox.svelte
+++ src/components/data-input/checkbox/checkbox.svelte
@@ -2,31 +2,24 @@
 	import type { CheckboxProps } from '.';
 	import { Check } from 'lucide-svelte';
 	import { Control, Description, FieldErrors, Label } from 'formsnap';
+	import { createCheckbox, melt } from '@melt-ui/svelte';
 
 	let { checked = $bindable(), label, description, ...props }: CheckboxProps = $props();
+	let inputEl: HTMLInputElement;
 </script>
 
-<!-- NOTICE: TO MAKE THIS FULLY WORKING INPUT NEEDS TO BE INSIDE OF THE BUTTON, BUT AS OF NOW IT'S IMPOSSIBLE MAKING THIS COMPONENT NOT FULLY WORKING WITH FORMSNAP -->
 <Control let:attrs>
 	<div class="flex ai:center gap:8">
 		<button
+			onclick={() => inputEl.click()}
 			class="flex ai:center jc:center font:18 b:2|solid|base-400 r:12 w:32 h:32 color:neutral"
-			onclick={() => {
-				checked = !checked;
-			}}
-			role="checkbox"
-			aria-checked={checked}
-			{...props}
-			{...attrs}
 		>
 			{#if checked}
-				<Check size={18} />
+				<Check size={24} />
 			{/if}
-
-			<!-- <input type="hidden" name={attrs.name} /> -->
 		</button>
 
-		<input type="hidden" name={attrs.name} />
+		<input type="checkbox" name={attrs.name} bind:checked bind:this={inputEl} hidden />
 
 		{#if label}
 			<Label class="f:18">
