f26027eb6c0c63e67f3eb41004d96b14ef3f9cca
dawidseipold <seipold.dawid@gmail.com>
2024-09-19 18:57:01 +0200

    feat: add common field valibot schemas

---
+++ src/lib/zod/schemas/auth/common.ts
@@ -0,0 +1,15 @@
+import * as v from 'valibot';
+
+export const EmailSchema = v.pipe(
+	v.string(),
+	v.nonEmpty('Please enter your email.'),
+	v.email('The email is badly formatted.')
+);
+
+export const PasswordSchema = v.pipe(v.string(), v.nonEmpty('Please enter your password.'));
+
+export const StrictPasswordSchema = v.pipe(
+	v.string(),
+	v.minLength(8, 'Password must be at least 8 characters long.'),
+	v.maxLength(64, 'Password must be at most 64 characters long.')
+);
