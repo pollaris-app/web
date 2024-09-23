e52369874c721296c8b3ff38fff4b9c1fc9b2423
dawidseipold <seipold.dawid@gmail.com>
2024-09-23 10:00:21 +0200

    feat: add styling to the auth-card chore: adjust justify-content of the auth layout

--- src/components/layout/auth-card/auth-card.svelte
+++ src/components/layout/auth-card/auth-card.svelte
@@ -5,8 +5,10 @@
 	let { title, description, children, footer }: AuthCardProps = $props();
 </script>
 
-<div class="flex flex:col gap:8">
-	<header>
+<div
+	class="flex flex:col gap:16 w:576 h:max-content bg:base-200 b:2|base-300|solid p:16 r:32 flex flex:col gap:32"
+>
+	<header class="flex flex:col gap:8 text:center">
 		<h1 class="f:32 fg:white f:bold">{title}</h1>
 		<p class="f:18">{description}</p>
 	</header>
@@ -16,10 +18,12 @@
 	</main>
 
 	{#if footer}
-		<footer>
+		<footer class="flex flex:col gap:16">
 			<Separator />
 
-			{@render footer()}
+			<div>
+				{@render footer()}
+			</div>
 		</footer>
 	{/if}
 </div>
--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <main class="bg:base-50 fg:neutral w:100% h:100dvh p:32">
-	<div class="max-w:1440 mx:auto flex gap:32 h:100%">
+	<div class="max-w:1440 mx:auto flex gap:32 h:100% jc:center">
 		{@render children()}
 	</div>
 </main>
